bdec9c9bdb1234327bac041496199860788422a5
besugo03 <alex.pizzuco@gmail.com>
2024-07-21 17:38:31 +0200

    importing and basic styling of FullCalendar

--- ToDoMMO/src/App.tsx
+++ ToDoMMO/src/App.tsx
@@ -5,6 +5,10 @@ import Header from './components/Header'
 import { MantineProvider } from '@mantine/core';
 import UserCard from './components/UserCard';
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
 function App() {
 
   return (
@@ -12,7 +16,8 @@ function App() {
     <MantineProvider>
       <Header title='' />
       <UserCard currentExperience={432} maxExperience={1000} username='Besugo' level={5} activeItemsNumber={12}></UserCard>
-      </MantineProvider>
+      <FullCalendar selectable={true} plugins={[dayGridPlugin]} initialView='dayGridMonth' />
+    </MantineProvider>
     </>
   )
 }
